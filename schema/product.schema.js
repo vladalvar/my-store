71b5d0c47d7303400097bc8f95960b199f0f5555
Vladimir Alvarez <gvladimiralvarez@gmail.com>
2022-07-15 13:17:12 -0600

    fix error in schema

--- schema/product.schema.js
+++ schema/product.schema.js
@@ -39,7 +39,7 @@ const queryProductSchema = Joi.object({
   offset,
   price,
   price_min,
-  price_max: price_min.when({
+  price_max: Joi.alternatives().conditional('price_min', {
     is: Joi.number().integer(),
     then: Joi.required()
   })
